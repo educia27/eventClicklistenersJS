12bbaafea13e724c738d7090ac211436c7f0a82c
Eddy Tamayo <edd1114@gmail.com>
2021-11-11 12:37:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function init () {
+    const missionAbort = document.getElementById("abortMission");
+    const button = document.getElementById("liftoffButton");
+    const paragraph = document.getElementById("statusReport");
+
+    // Put your code for the exercises here.
+    //console.dir(document);
+}
+
+window.addEventListener("load", init);
